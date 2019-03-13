01a13cc640c0974e29c2d9e7a9c55f21705ca2d4
Joanne Chen <joanne.chen@berkeley.edu>
2019-03-13 12:10:04 -0700

    deleted extra file

--- Code/motif_pipeline.py
+++ Code/motif_pipeline.py
@@ -7,10 +7,8 @@ def bash_pipeline(alignment_file, motif_file, stand_cutoff=-50):
 	motif_file: motif file name, for example "bcd_FlyReg.fm"
 	stand_cutoff: score threshold (default no threshold)
 	"""
-	align_path = "../../TFBS_presence/data/alignments/align_outlier_rm_with_length_" + alignment_file
-	motif_path = "../../TFBS_presence/data/pwm/" + motif_file
 	df = prelim_pipeline(align_path, motif_path)
-	df.to_csv("../data/" + alignment_file[:-3] + "_bash_output")
+	df.to_csv(alignment_file + motif_file + "_bash_output")
 	# tacks on the alignment file name without ".fa" for the name of the csv output
 
 bash_pipeline(sys.argv[1], sys.argv[2])
